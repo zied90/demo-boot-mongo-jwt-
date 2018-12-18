fcf334a245de1ae2c0590017087442ee26645fd1
zied90 <miladi.medamine@gmail.com>
2018-12-18 11:18:01 +0100

    initial commit: basic structure

---
+++ src/main/java/com/cab/demobootmongojwt/DemoBootMongoJwtApplication.java
@@ -0,0 +1,14 @@
+package com.cab.demobootmongojwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootMongoJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBootMongoJwtApplication.class, args);
+    }
+
+}
+
